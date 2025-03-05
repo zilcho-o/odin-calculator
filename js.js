f5e9f2a19e8fadec90f20134f9eed51ac1b8142f
zilchox <164333718+zilcho-o@users.noreply.github.com>
2025-03-05 13:35:37 +0800

    Added calculator interface

--- js.js
+++ js.js
@@ -1,15 +1,32 @@
-function add() {
-
+function add(a, b) {
+    return a + b;
 }
 
-function minus() {
-    
+function minus(a, b) {
+    return a - b;
 }
 
-function multiply() {
-    
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide() {
-    
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            minus(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
 }
